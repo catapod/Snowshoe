8e55e264d9870d70b9027c3d6b9724496e06cc55
Dmitry Samsonov <user1736@yandex.ru>
2016-07-11 14:12:22 +0300

    fix code alignment

--- src/reducers/slices.js
+++ src/reducers/slices.js
@@ -3,19 +3,19 @@ import { assign } from 'lodash';
 
 export default (state = {}, action) => {
     switch (action.type) {
-    case actionTypes.ADD_TRANSLATION:
-        const sliceId = action.id;
-        const currentSlice = state[action.id];
+        case actionTypes.ADD_TRANSLATION:
+            const sliceId = action.id;
+            const currentSlice = state[action.id];
 
-        return assign({}, state, {
-            [sliceId]: assign({}, currentSlice, {
-                translations: [
-                    ...currentSlice.translations,
-                    action.translateId
-                ]
-            })
-        });
-    default:
-        return state;
+            return assign({}, state, {
+                [sliceId]: assign({}, currentSlice, {
+                    translations: [
+                        ...currentSlice.translations,
+                        action.translateId
+                    ]
+                })
+            });
+        default:
+            return state;
     }
 };
