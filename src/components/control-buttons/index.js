23e4a88596c04affa31003d07d6c19bfad7d0b9a
Mihail <mihail.nekrytov@gmail.com>
2017-04-17 20:18:54 +0300

    Add drop-down help in menu

--- src/components/control-buttons/index.js
+++ src/components/control-buttons/index.js
@@ -1,10 +1,75 @@
 import React from 'react';
+import '../../common/styles/drop-menu.less';
 
 export default class ControlButtons extends React.Component {
     render() {
         return (
             <div className='content__control-buttons'>
-                <div className='content__command-key'>⌘</div>
+                <div className='content__command-key drop-menu__link'>
+                    ⌘
+                    <div className='drop-menu__menu hotkeys'>
+
+                        <div className='hotkeys__group'>
+                            <p className='hotkeys__title'>Группа команд</p>
+                            <div className='hotkeys__command'>
+                                <span className='hotkeys__key'>Ctrl</span>
+                                <span className='hotkeys__plus'>+</span>
+                                <span className='hotkeys__key'>C</span>
+                                <span className='hotkeys__do'>Команда</span>
+                            </div>
+                            <div className='hotkeys__command'>
+                                <span className='hotkeys__key'>Ctrl</span>
+                                <span className='hotkeys__plus'>+</span>
+                                <span className='hotkeys__key'>C</span>
+                                <span className='hotkeys__do'>Команда</span>
+                            </div>
+                            <div className='hotkeys__command'>
+                                <span className='hotkeys__key'>Ctrl</span>
+                                <span className='hotkeys__plus'>+</span>
+                                <span className='hotkeys__key'>C</span>
+                                <span className='hotkeys__do'>Команда</span>
+                            </div>
+                        </div>
+
+                        <div className='hotkeys__group'>
+                            <p className='hotkeys__title'>Группа команд</p>
+                            <div className='hotkeys__command'>
+                                <span className='hotkeys__key'>Ctrl</span>
+                                <span className='hotkeys__plus'>+</span>
+                                <span className='hotkeys__key'>C</span>
+                                <span className='hotkeys__do'>Команда</span>
+                            </div>
+                            <div className='hotkeys__command'>
+                                <span className='hotkeys__key'>Ctrl</span>
+                                <span className='hotkeys__plus'>+</span>
+                                <span className='hotkeys__key'>C</span>
+                                <span className='hotkeys__do'>Команда</span>
+                            </div>
+                            <div className='hotkeys__command'>
+                                <span className='hotkeys__key'>Ctrl</span>
+                                <span className='hotkeys__plus'>+</span>
+                                <span className='hotkeys__key'>C</span>
+                                <span className='hotkeys__do'>Команда</span>
+                            </div>
+                        </div>
+
+                        <div className='hotkeys__group'>
+                            <p className='hotkeys__title'>Группа команд</p>
+                            <div className='hotkeys__command'>
+                                <span className='hotkeys__key'>Ctrl</span>
+                                <span className='hotkeys__plus'>+</span>
+                                <span className='hotkeys__key'>C</span>
+                                <span className='hotkeys__do'>Команда</span>
+                            </div>
+                            <div className='hotkeys__command'>
+                                <span className='hotkeys__key'>Ctrl</span>
+                                <span className='hotkeys__plus'>+</span>
+                                <span className='hotkeys__key'>C</span>
+                                <span className='hotkeys__do'>Команда</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className='content__view-key content__view-key--active'></div>
             </div>
         );
