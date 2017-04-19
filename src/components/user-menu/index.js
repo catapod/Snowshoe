65a8dcf57bf3b022b647de2f23a916d75b891926
Mihail <mihail.nekrytov@gmail.com>
2017-04-20 02:46:00 +0300

    Fix name of user-menu

--- src/components/user-menu/index.js
+++ src/components/user-menu/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default class UserMenu extends React.Component {
     render() {
         return (
-            <a className='header__user'>
-                <div className='header__avatar'></div>
-                <p className='header__username'>Hvostik12</p>
+            <a className='user-menu'>
+                <div className='user-menu__avatar'></div>
+                <p className='user-menu__username'>Hvostik12</p>
             </a>
         );
     }
