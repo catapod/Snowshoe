f7fb421e68bb04ac2bb746bbc7ff125392539a56
Дмитрий Басавин <basavind@gmail.com>
2017-04-23 14:32:12 +0300

    Rename sidebar-controls__item to sidebar-control

--- src/components/sidebar-controls/index.js
+++ src/components/sidebar-controls/index.js
@@ -4,10 +4,10 @@ export default class SidebarControls extends React.Component {
     render() {
         return (
             <div className='sidebar-controls'>
-                <a className='sidebar-controls__item sidebar-controls__dictionary'></a>
-                <a className='sidebar-controls__item sidebar-controls__comments sidebar-controls__item--active'></a>
-                <a className='sidebar-controls__item sidebar-controls__users'></a>
-                <a className='sidebar-controls__item sidebar-controls__info'></a>
+                <a className='sidebar-control sidebar-control__dictionary'></a>
+                <a className='sidebar-control sidebar-control__comments sidebar-control--active'></a>
+                <a className='sidebar-control sidebar-control__users'></a>
+                <a className='sidebar-control sidebar-control__info'></a>
             </div>
         );
     }
