1129e981c445f0b3951cbb57a8f8dd2dff7e251a
Дмитрий Басавин <basavind@gmail.com>
2017-04-23 13:30:57 +0300

    Remove pluralization for single document title

--- src/components/document-titles/index.js
+++ src/components/document-titles/index.js
@@ -4,8 +4,8 @@ export default class DocumentTitles extends React.Component {
     render() {
         return (
             <div className='document-titles'>
-                <h1 className='document-titles__origin'>Document name</h1>
-                <h1 className='document-titles__translate'>Название документа</h1>
+                <h1 className='document-title__origin'>Document name</h1>
+                <h1 className='document-title__translate'>Название документа</h1>
             </div>
         );
     }
