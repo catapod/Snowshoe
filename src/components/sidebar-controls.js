37526a82c6f6219331bdc14149ce5ebaf310ce36
Дмитрий Басавин <basavind@gmail.com>
2017-05-05 14:21:22 +0300

    rename sidebar sections icons

--- src/components/sidebar-controls.js
+++ src/components/sidebar-controls.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../styles/sidebar-controls.less';
-import commentsWhite from '../assets/comments-white.svg';
-import iconDictionary from '../assets/dictionary.svg';
-import iconInfo from '../assets/info.svg';
-import iconUsers from '../assets/users.svg';
+import iconSidebarComments from '../assets/sidebar-comments.svg';
+import iconSidebarDictionary from '../assets/sidebar-dictionary.svg';
+import iconSidebarInfo from '../assets/sidebar-info.svg';
+import iconSidebarUsers from '../assets/sidebar-users.svg';
 
 
 function SidebarControls() {
@@ -11,19 +11,19 @@ function SidebarControls() {
         <div className='sidebar-controls'>
             <a
                 className='sidebar-control sidebar-control--dictionary'
-                dangerouslySetInnerHTML={{__html: iconDictionary}}
+                dangerouslySetInnerHTML={{__html: iconSidebarDictionary}}
             />
             <a
                 className='sidebar-control sidebar-control--comments sidebar-control--active'
-                dangerouslySetInnerHTML={{__html: commentsWhite}}
+                dangerouslySetInnerHTML={{__html: iconSidebarComments}}
             />
             <a
                 className='sidebar-control sidebar-control--users'
-                dangerouslySetInnerHTML={{__html: iconUsers}}
+                dangerouslySetInnerHTML={{__html: iconSidebarUsers}}
             />
             <a
                 className='sidebar-control sidebar-control--info'
-                dangerouslySetInnerHTML={{__html: iconInfo}}
+                dangerouslySetInnerHTML={{__html: iconSidebarInfo}}
             />
         </div>
     );
