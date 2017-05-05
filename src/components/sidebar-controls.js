a8370a90a6e546cea077c35f0c35235cb775a67c
Дмитрий Басавин <basavind@gmail.com>
2017-05-05 12:37:26 +0300

    inline users icon

--- src/components/sidebar-controls.js
+++ src/components/sidebar-controls.js
@@ -3,6 +3,7 @@ import '../styles/sidebar-controls.less';
 import commentsWhite from '../assets/comments-white.svg';
 import iconDictionary from '../assets/dictionary.svg';
 import iconInfo from '../assets/info.svg';
+import iconUsers from '../assets/users.svg';
 
 
 function SidebarControls() {
@@ -16,7 +17,10 @@ function SidebarControls() {
                 className='sidebar-control sidebar-control--comments sidebar-control--active'
                 dangerouslySetInnerHTML={{__html: commentsWhite}}
             />
-            <a className='sidebar-control sidebar-control--users' />
+            <a
+                className='sidebar-control sidebar-control--users'
+                dangerouslySetInnerHTML={{__html: iconUsers}}
+            />
             <a
                 className='sidebar-control sidebar-control--info'
                 dangerouslySetInnerHTML={{__html: iconInfo}}
