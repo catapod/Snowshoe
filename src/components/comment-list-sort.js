f6f93c2ca0464baa07e4e53bff9de0c5ff4759b1
Дмитрий Басавин <basavind@gmail.com>
2017-05-05 12:50:33 +0300

    inline sort icon

--- src/components/comment-list-sort.js
+++ src/components/comment-list-sort.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/comment-list-sort.less';
+import iconSort from '../assets/sort.svg';
 
 
 function CommentListSort() {
     return (
         <div className='comment-list-sort comment-list-sort__droplink'>
+            <i dangerouslySetInnerHTML={{__html: iconSort}} />
             <div className='comment-list-sort__dropdown'>
                 <label className='comment-list-sort__label'>
                     <input type='radio' name='comments-sort' /> Сначала новые
