7c119052b762d908dfc7df017719f8ffb18de614
Дмитрий Басавин <basavind@gmail.com>
2017-05-05 12:32:22 +0300

    inline dictionary icon

--- src/components/sidebar-controls.js
+++ src/components/sidebar-controls.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../styles/sidebar-controls.less';
 import commentsWhite from '../assets/comments-white.svg';
+import iconDictionary from '../assets/dictionary.svg';
 
 
 function SidebarControls() {
     return (
         <div className='sidebar-controls'>
-            <a className='sidebar-control sidebar-control--dictionary' />
+            <a
+                className='sidebar-control sidebar-control--dictionary'
+                dangerouslySetInnerHTML={{__html: iconDictionary}}
+            />
             <a
                 className='sidebar-control sidebar-control--comments sidebar-control--active'
                 dangerouslySetInnerHTML={{__html: commentsWhite}}
