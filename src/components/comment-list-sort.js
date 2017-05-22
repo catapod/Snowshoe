11009a1381243e1b36a17e47828d377dcc0b683f
Mihail <mihail.nekrytov@gmail.com>
2017-05-22 04:45:00 +0300

    Replace tag-selectors with class-selectors for radio

--- src/components/comment-list-sort.js
+++ src/components/comment-list-sort.js
@@ -5,13 +5,13 @@ import iconSort from '../assets/sort.svg';
 
 function CommentListSort() {
     return (
-        <div className='comment-list-sort comment-list-sort__droplink'>
+        <div className='comment-list-sort comment-list-sort__droplink comment-list-sort__droplink'>
             <i dangerouslySetInnerHTML={{__html: iconSort}} />
             <div className='comment-list-sort__dropdown'>
                 <input id='first-new' type='radio' name='comments-sort' className='comment-list-sort__input'/>
-                <label htmlFor='first-new'>Сначала новые</label>
+                <label htmlFor='first-new' className='radio-buttons__label'>Сначала новые</label>
                 <input id='first-old' type='radio' name='comments-sort' className='comment-list-sort__input'/>
-                <label htmlFor='first-old'>Сначала старые</label>
+                <label htmlFor='first-old' className='radio-buttons__label'>Сначала старые</label>
             </div>
         </div>
     );
