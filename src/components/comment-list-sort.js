28eb87e296a871bdb731efbed3d94c85efc4bf7d
Aleksey Gurianov <gurianov@gmail.com>
2017-07-01 21:51:02 +0000

    💄fix linting warnings

--- src/components/comment-list-sort.js
+++ src/components/comment-list-sort.js
@@ -8,9 +8,9 @@ function CommentListSort() {
         <div className='comment-list-sort comment-list-sort__droplink comment-list-sort__droplink'>
             <i dangerouslySetInnerHTML={{__html: iconSort}} />
             <div className='comment-list-sort__dropdown'>
-                <input id='first-new' type='radio' name='comments-sort' className='comment-list-sort__input'/>
+                <input id='first-new' type='radio' name='comments-sort' className='comment-list-sort__input' />
                 <label htmlFor='first-new' className='radio-buttons__label'>Сначала новые</label>
-                <input id='first-old' type='radio' name='comments-sort' className='comment-list-sort__input'/>
+                <input id='first-old' type='radio' name='comments-sort' className='comment-list-sort__input' />
                 <label htmlFor='first-old' className='radio-buttons__label'>Сначала старые</label>
             </div>
         </div>
